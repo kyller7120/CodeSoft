e9c99ab0f1bb73b52671634b998a79045f336bac
kyller7120 <riveramarlon113@gmail.com>
2023-10-25 17:39:32 -0600

    despliegue4

--- render/build.sh
+++ render/build.sh
@@ -4,4 +4,4 @@ python -m pip install -r ./requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('proyectosic', 'proyecto123', 'proyecto123')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('proyectosic', 'proyectosic', 'proyectosic')" | python manage.py shell
