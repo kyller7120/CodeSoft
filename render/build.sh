101ea8c96d3ce32f6fa28048a51033a56bf13c40
Marlon Rivera <131151048+kyller7120@users.noreply.github.com>
2023-11-07 02:58:27 -0600

    Update build.sh
    prueba

--- render/build.sh
+++ render/build.sh
@@ -2,6 +2,7 @@ set -o errexit
 
 python -m pip install -r ./requirements.txt
 
+python manage.py loaddata initProject.json
 python manage.py collectstatic --no-input
 python manage.py migrate
 #echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('proyectosic', 'proyectosic', 'proyectosic')" | python manage.py shell
