54356d21645479584489cbc1ebf701881d1b4c3b
Marlon Rivera <131151048+kyller7120@users.noreply.github.com>
2023-11-07 04:41:42 -0600

    Update build.sh

--- render/build.sh
+++ render/build.sh
@@ -4,8 +4,8 @@ python -m pip install -r ./requirements.txt
 
 python manage.py loaddata initProject.json
 python manage.py collectstatic --no-input
-python manage.py migrate codesoft zero
+python manage.py migrate codesoftapp zero
 
-python manage.py makemigrations codesoft
+python manage.py makemigrations codesoftapp
 python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('proyectosic', 'proyectosic', 'proyectosic')" | python manage.py shell
